7724dbfcad744604a0f6cb40e11c26e8891eb6b4
Castilho, N <nuno_castilho78@hotmail.com>
2018-12-01 22:38:42 +0000

    Added vendor files from template

--- frontend/src/components/NotFoundPage/NotFoundPage.js
+++ frontend/src/components/NotFoundPage/NotFoundPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFoundPage = () => {
   return (
-    <div className='g-min-height-100vh g-flex-centered g-pa-15'>
+    <div className='g-min-height-90vh g-flex-centered g-pa-15'>
       <div className='text-center g-flex-centered-item g-position-rel g-pb-15'>
         <div className='g-font-size-180 g-font-size-240--sm g-line-height-1 g-font-weight-600er g-color-gray-light-v4'>404</div>
         <div className='g-absolute-centered'>
