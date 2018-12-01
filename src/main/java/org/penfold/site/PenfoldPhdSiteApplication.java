0ce1ac516b20c82430788ec33e3a7710c67f842e
Castilho, N <nuno_castilho78@hotmail.com>
2018-12-01 10:13:39 +0000

    Initial commit

---
+++ src/main/java/org/penfold/site/PenfoldPhdSiteApplication.java
@@ -0,0 +1,12 @@
+package org.penfold.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenfoldPhdSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PenfoldPhdSiteApplication.class, args);
+    }
+}
